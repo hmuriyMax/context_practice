e028b2a71fba4a1413fe5389bb9a7f765efcb113
max <mshchemilkin@gmail.com>
2022-06-24 17:15:34 +0300

    [Day 5] Added simple test

--- cmd/simple_test/main.go
+++ cmd/simple_test/main.go
@@ -3,9 +3,62 @@ package main
 import (
 	"context"
 	"fmt"
+	"log"
+	"os"
+	"sync"
+	"time"
 )
 
+var wg = sync.WaitGroup{}
+
+//Функция, которая запишет во второй параметр true, когда контекст закроется
+func checkCtx(ctx context.Context, res *bool) {
+	*res = false
+	select {
+	case <-ctx.Done():
+		*res = true
+	}
+}
+
+//Какой-то процесс, выводящий каждую секунду сообщение.
+func process(ctx context.Context) {
+	ctxClosed := false
+	go checkCtx(ctx, &ctxClosed)
+
+	//Выводим сообщение о том, как завершилась функция: сама или при закрытии контекста
+	defer func() {
+		if ctxClosed {
+			fmt.Printf("Context cancelled: finishing")
+		} else {
+			fmt.Printf("Planned finish!")
+		}
+		wg.Done()
+	}()
+
+	//Сама работа функции
+	for i := 0; i < 8; i++ {
+		fmt.Printf("%ds passed\n", i)
+		time.Sleep(time.Second)
+		if ctxClosed {
+			return
+		}
+	}
+}
+
 func main() {
-	var ctx context.Context
-	fmt.Printf("%s", ctx.Value("key"))
+	ctx := context.Background()
+	ctx, cancelFunction := context.WithCancel(ctx)
+
+	wg.Add(1)
+	//Запускаем процесс в горутине
+	go process(ctx)
+	//Ожидаем ввода пользователя
+	_, err := fmt.Fscanln(os.Stdin)
+	if err != nil {
+		log.Fatalf(err.Error())
+	}
+	//После ввода вызываем функцию отмены. Таким образом, как только пользователь нажал enter, контекст отменяется
+	cancelFunction()
+	//Ожидаем завершения вейт-группы
+	wg.Wait()
 }
